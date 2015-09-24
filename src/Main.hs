0b83405d5691840e18da5289f4c1e800786c440d
alaya <lvwenlong_lambda@qq.com>
2015-09-24 14:37:47 +0800

    Parser of Brainfuck

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 import Text.ParserCombinators.Parsec
-import Control.Applicative
+import Control.Applicative hiding((<|>), many)
 
 data MoveList a = MoveList {
     preList :: [a],
@@ -19,14 +19,39 @@ next :: MoveList a -> MoveList a
 next (MoveList pre (c:cs)) = MoveList (c:pre) cs
 prev :: MoveList a -> MoveList a
 prev (MoveList (p:ps) curr) = MoveList ps (p:curr)
-
-
-
 addOne :: (Enum a) => MoveList a -> MoveList a
 addOne = headApply succ
-
 subOne :: (Enum a) => MoveList a -> MoveList a
 subOne = headApply pred
+
+data BrainFuck = RightMove 
+               | LeftMove
+               | Add
+               | Sub
+               | Output
+               | Input
+               | Loop [BrainFuck]
+               deriving(Eq, Show)
+
+parseBrainFuck :: Parser [BrainFuck]
+parseBrainFuck = many parseBrainFuckOp
+  where parseBrainFuckOp = RightMove <$ char '>'
+                       <|> LeftMove  <$ char '<'
+                       <|> Add       <$ char '+'
+                       <|> Sub       <$ char '-'
+                       <|> Output    <$ char '.'
+                       <|> Input     <$ char ','
+                       <|> Loop <$> between (char '[') (char ']') parseBrainFuck
+                       <?> "BRAINFUCK EXPRESSION"
+-- parseBrainFuck = many $ RightMove <$ char '>'
+--                     <|> LeftMove  <$ char '<'
+--                     <|> Add       <$ char '+'
+--                     <|> Sub       <$ char '-'
+--                     <|> Output    <$ char '.'
+--                     <|> Input     <$ char ','
+--                     <|> Loop <$> between (char '[') (char ']') parseBrainFuck
+--                     <?> "BRAINFUCK EXPRESSION"
+
 
 
 main = undefined
