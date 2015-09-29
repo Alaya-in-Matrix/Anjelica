36545038c5217ad88768334e5dd4adf15d715276
alaya <lvwenlong_lambda@qq.com>
2015-09-29 23:29:33 +0800

    Parser for Whitespace programming language

--- src/Language/Whitespace.hs
+++ src/Language/Whitespace.hs
@@ -2,19 +2,98 @@ module Language.Whitespace where
 
 import Text.ParserCombinators.Parsec
 import Control.Monad
-import Control.Applicative hiding((<|>))
+import Control.Applicative hiding((<|>), many)
 import System.Environment
 
-data IMP = StackManip | Arithmetic | HeapAccess | FlowCtrl | IO deriving(Show, Eq)
+data IMP = Stack      StackOp
+         | Arithmetic ArithOp
+         | Heap       HeapOp
+         | Control    CtrlCmd
+         | IO         IOCmd deriving(Show, Eq)
+
+data StackOp = Push Integer | Top  | Copy Integer | Swap | Pop | Slide Integer deriving(Show, Eq)
+data ArithOp = Add | Sub | Mul | Div | Mod deriving(Eq, Show)
+data HeapOp  = Store | Retrieve deriving(Eq, Show)
+data IOCmd   = OutChar | OutInt | ReadChar | ReadInt deriving(Eq, Show)
+data CtrlCmd = Label Integer | Call Integer | Jump Integer | JumpZero Integer | JumpNeg Integer | FuncEnd | ProgEnd deriving(Eq, Show)
 
 wsSpace = char ' '
 wsTab   = char '\t'
 wsLf    = char '\n'
 
 impParser :: Parser IMP
-impParser = (FlowCtrl <$ wsLf) 
-        <|> (StackManip <$ wsSpace) 
-        <|> (wsTab *> (Arithmetic <$ wsSpace 
-                    <|> HeapAccess <$ wsTab 
-                    <|> IO <$ wsLf))
+impParser = wsLf *> (Control  <$> ctrlCmdParser)
+        <|> wsSpace *> (Stack <$> stackOpParser)
+        <|> wsTab   *> (wsSpace *> (Arithmetic <$> arithOpParser) <|> 
+                        wsTab   *> (Heap <$> heapOpParser)        <|>
+                        wsLf    *> (IO <$> ioCmdParser))
 
+numParser :: Parser Integer
+numParser = ((*) <$> signParser <*> intParser) <* wsLf
+  where signParser    = 1 <$ wsSpace <|> (-1) <$ wsTab
+        bitParser     = 0 <$ wsSpace <|> 1    <$ wsTab
+        intParser     = bitsToInteger <$> (many bitParser)
+        bitsToInteger = foldl ((+) . (*2)) 0
+
+stackOpParser :: Parser StackOp 
+stackOpParser = do
+    firstChar <- oneOf " \t\n"
+    case firstChar of
+      ' '  -> Push <$> numParser
+      '\t' -> ((Copy <$ wsSpace) <|> (Slide <$ wsLf)) <*> numParser
+      '\n' -> Top  <$ wsSpace <|> 
+              Swap <$ wsTab   <|> 
+              Pop  <$ wsLf
+
+arithOpParser :: Parser ArithOp
+arithOpParser = wsSpace *> (Add <$ wsSpace <|> Sub <$ wsTab <|> Mul <$ wsLf)
+            <|> wsTab   *> (Div <$ wsSpace <|> Mod <$ wsTab)
+
+heapOpParser :: Parser HeapOp
+heapOpParser = Store <$ wsSpace <|> Retrieve <$ wsTab
+
+ioCmdParser :: Parser IOCmd
+ioCmdParser = wsSpace *> (OutChar  <$ wsSpace <|> OutInt  <$ wsTab)
+          <|> wsTab   *> (ReadChar <$ wsSpace <|> ReadInt <$ wsTab)
+
+ctrlCmdParser :: Parser CtrlCmd
+ctrlCmdParser = wsSpace *> (wsSpace *> (Label <$> numParser) <|>
+                            wsTab   *> (Call  <$> numParser) <|>
+                            wsLf    *> (Jump  <$> numParser))
+            <|> wsTab   *> (wsSpace *> (JumpZero <$> numParser) <|>
+                            wsTab   *> (JumpNeg  <$> numParser) <|>
+                            FuncEnd <$ wsLf)
+            <|> FuncEnd <$ (wsLf *> wsLf)
+-- num   = "\t \t\t\n" -- (-3)
+-- testInstructions = [
+--     " "    ++ num 
+--   , " "    ++ "\n " 
+--   , " "    ++ "\t "  ++ num 
+--   , " "    ++ "\n\t" 
+--   , " "    ++ "\n\n" 
+--   , " "    ++ "\t\n" ++ num
+
+--   , "\t "  ++ "  "
+--   , "\t "  ++ " \t"
+--   , "\t "  ++ " \n"
+--   , "\t "  ++ "\t "
+--   , "\t "  ++ "\t\t"
+
+--   , "\t\t" ++ "\t"
+--   , "\t\t" ++ " "
+
+--   , "\t\n" ++ " \t"
+--   , "\t\n" ++ "  "
+--   , "\t\n" ++ "\t\t"
+--   , "\t\n" ++ "\t "
+
+--   , "\n"   ++ "  "   ++ num
+--   , "\n"   ++ " \t"  ++ num
+--   , "\n"   ++ " \n"  ++ num
+--   , "\n"   ++ "\t "  ++ num
+--   , "\n"   ++ "\t\t" ++ num
+--   , "\n"   ++ "\t\n"
+--   , "\n"   ++ "\n\n"
+--   ]
+-- test :: IO ()
+-- test = mapM_ (parseTest impParser) testInstructions
--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,7 @@ import Text.ParserCombinators.Parsec
 import Control.Applicative hiding((<|>), many)
 import Control.Monad
 import System.Environment
-import Language.Whitespace
+import qualified Language.Whitespace as W
 
 -- both preList and currentList are supposed to be infinite list
 -- so I won't consider the situation of empty list
