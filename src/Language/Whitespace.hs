e7d0ab56b726f05cb1080ea753df0ba24f36660d
alaya <lvwenlong_lambda@qq.com>
2015-10-06 00:38:22 +0800

    Refactoring...

--- src/Language/Whitespace.hs
+++ src/Language/Whitespace.hs
@@ -1,7 +1,7 @@
 module Language.Whitespace where
 
 -- Author: lvwenlong_lambda@qq.com
--- Last Modified:2015年10月05日 星期一 22时45分52秒 一
+-- Last Modified:2015年10月06日 星期二 00时37分45秒 二
 
 import Text.ParserCombinators.Parsec
 import Control.Monad
@@ -13,7 +13,8 @@ import Data.Char(intToDigit)
 import qualified Data.Vector as V
 import qualified Data.Map.Strict as M hiding ((!))
 
-(!) = (V.!)
+(!)  = (V.!)
+(!?) = (V.!?)
 
 data IMP     = Stack      StackOp
              | Arithmetic ArithOp
@@ -144,7 +145,8 @@ data VM = VM {
     heap :: M.Map Integer Integer,
     instructions :: V.Vector IMP , -- This should be an array(vector) instead of a list
     pc :: Int , 
-    jumptable :: M.Map Integer Int
+    jumptable :: M.Map Integer Int,
+    retStack :: [Integer]
 } deriving(Eq, Show)
 
 initVM :: V.Vector IMP -> VM
@@ -152,7 +154,18 @@ initVM instrs = let stack     = []
                     heap      = M.empty
                     pc        = 0
                     jumptable = M.empty
-                 in VM stack heap instrs pc jumptable
+                    retStack  = []
+                 in VM stack heap instrs pc jumptable retStack
+
+newStack f (VM s h i p j r) = VM (f s) h i p j r
+newHeap  f (VM s h i p j r) = VM s (f h) i p j r
+newInstr f (VM s h i p j r) = VM s h (f i) p j r
+newPC    f (VM s h i p j r) = VM s h i (f p) j r
+newJumpT f (VM s h i p j r) = VM s h i p (f j) r
+newRetS  f (VM s h i p j r) = VM s h i p j (f r)
+
+incPC :: VM -> IO VM
+incPC vm = return $ newPC (+1) vm
 
 
 runWhiteSpace :: FilePath -> IO ()
@@ -161,56 +174,65 @@ runWhiteSpace file = do
     case parse (many impParser) "Whitespace" content of
       Left  err -> print err
       Right val -> do mapM_ (putStrLn.show) val
+                      putStrLn "=============================="
                       eval $ initVM (V.fromList val)
                       putStr "\n"
 
 
 eval :: VM -> IO VM
-eval vm = let index   = pc vm
+eval vm = let index = pc vm
               codeMem = instructions vm
-              size    = length codeMem
-           in if index >= size
-                 then return vm
-                 else let instr = codeMem ! index
-                       in case instr of
-                            Control ProgEnd -> return vm
-                            _               -> evalInstr vm instr >>= eval
-
+              instr = codeMem !? index
+           in case instr of 
+                Nothing                 -> return vm
+                Just (Control ProgEnd)  -> return vm
+                Just ins                -> evalInstr vm ins >>= eval
 
 evalInstr :: VM -> IMP -> IO VM
-evalInstr (VM s h i p j) (Stack (Push num)) = return $ VM (num:s)  h i (p+1) j
-evalInstr (VM s h i p j) (Stack Duplicate)  = undefined
-evalInstr (VM s h i p j) (Stack Pop)        = return $ VM (tail s) h i (p+1) j
-evalInstr (VM s h i p j) (Stack Swap)       = return $ VM (swap s) h i (p+1) j 
-  where swap (a:b:rest) = (b:a:rest)
-evalInstr (VM (x:y:rest) h i p j) (Arithmetic arOp) = return $ VM ((x `op` y):rest) h i (p+1) j 
-  where op = case arOp of
-               Add -> (+)
-               Sub -> (-)
-               Mul -> (*)
-               Div -> div
-               Mod -> mod
-evalInstr (VM (val:addr:rest) h i p j) (Heap Store) = return $ VM rest newHeap i (p+1) j
-  where newHeap = M.insert addr val h 
-evalInstr (VM (addr:rest) h i p j) (Heap Retrieve)  = return $ VM (val:rest) h i (p+1) j
-  where val = fromJust $ M.lookup addr h
-evalInstr vm@(VM s h i p j) (IO OutChar)  = (VM s h i (p+1) j)<$ (putChar . toEnum . fromEnum . head) s
-evalInstr vm@(VM s h i p j) (IO OutInt)   = (VM s h i (p+1) j)<$  (putStr  . show   . fromEnum . head) s
-evalInstr vm@(VM s h i p j) (IO ReadChar) = do int <- (toInteger.fromEnum) <$> getChar
-                                               return $ VM (int:s) h i (p+1) j
-evalInstr vm@(VM s h i p j) (IO ReadInt)  = do int <- read <$> getLine
-                                               return $ VM (int:s) h i (p+1) j
-evalInstr (VM s h i p j) (Control (Label n)) = return $ VM s h i (p+1) newJump
-  where newJump = M.insert n (p+1) j
-evalInstr (VM s h i p j) (Control (Call label)) = undefined
-evalInstr (VM s h i p j) (Control FuncEnd)      = undefined
-evalInstr (VM s h i p j) (Control ProgEnd)      = undefined
-evalInstr (VM s h i p j) (Control (Jump n)) = return $ VM s h i newPC j
-  where newPC = fromJust $ M.lookup n j
-evalInstr (VM s h i p j) (Control (JumpNeg n)) = return $ VM s h i newPC j
-  where newPC = if ((head s) < 0) then fromJust $ M.lookup n j
-                                  else p+1
-evalInstr (VM s h i p j) (Control (JumpZero n)) = return $ VM s h i newPC j
-  where newPC = if ((head s) == 0) then fromJust $ M.lookup n j
-                                   else p+1
+evalInstr vm (Stack (Push num)) = incPC $ newStack (num:) vm
+evalInstr vm (Stack Duplicate)  = undefined -- not supported yet
+evalInstr vm (Stack Pop)        = incPC $ newStack tail vm
+evalInstr vm (Stack Swap)       = incPC $ newStack swapTop vm
+  where swapTop []  = error "empty list"
+        swapTop [a] = error "single element list"
+        swapTop (a:b:xs) = (b:a:xs)
+evalInstr vm (Arithmetic arithOp) = incPC $ newStack op vm
+  where op []         = error "empty list"
+        op [x]        = error "single element list"
+        op (a:b:rest) = (a `fuck` b) : rest
+        fuck          = case arithOp of Add -> (+)
+                                        Sub -> (-)
+                                        Mul -> (*)
+                                        Div -> div
+                                        Mod -> mod
+evalInstr vm (Heap Store) = incPC $ newHeap (M.insert addr val) vm
+  where (addr:val:_) = stack vm
+evalInstr vm (Heap Retrieve) = incPC $ newStack retrieve vm
+  where retrieve (addr:rest) = let val = M.lookup addr (heap vm)
+                                 in (fromJust val) : rest
+
+-- evalInstr :: VM -> IMP -> IO VM
+-- evalInstr (VM (val:addr:rest) h i p j) (Heap Store) = return $ VM rest newHeap i (p+1) j
+--   where newHeap = M.insert addr val h 
+-- evalInstr (VM (addr:rest) h i p j) (Heap Retrieve)  = return $ VM (val:rest) h i (p+1) j
+--   where val = fromJust $ M.lookup addr h
+-- evalInstr vm@(VM s h i p j) (IO OutChar)  = (VM s h i (p+1) j)<$ (putChar . toEnum . fromEnum . head) s
+-- evalInstr vm@(VM s h i p j) (IO OutInt)   = (VM s h i (p+1) j)<$  (putStr  . show   . fromEnum . head) s
+-- evalInstr vm@(VM s h i p j) (IO ReadChar) = do int <- (toInteger.fromEnum) <$> getChar
+--                                                return $ VM (int:s) h i (p+1) j
+-- evalInstr vm@(VM s h i p j) (IO ReadInt)  = do int <- read <$> getLine
+--                                                return $ VM (int:s) h i (p+1) j
+-- evalInstr (VM s h i p j) (Control (Label n)) = return $ VM s h i (p+1) newJump
+--   where newJump = M.insert n (p+1) j
+-- evalInstr (VM s h i p j) (Control (Call label)) = undefined
+-- evalInstr (VM s h i p j) (Control FuncEnd)      = undefined
+-- evalInstr (VM s h i p j) (Control ProgEnd)      = undefined
+-- evalInstr (VM s h i p j) (Control (Jump n)) = return $ VM s h i newPC j
+--   where newPC = fromJust $ M.lookup n j
+-- evalInstr (VM s h i p j) (Control (JumpNeg n)) = return $ VM s h i newPC j
+--   where newPC = if ((head s) < 0) then fromJust $ M.lookup n j
+--                                   else p+1
+-- evalInstr (VM s h i p j) (Control (JumpZero n)) = return $ VM s h i newPC j
+--   where newPC = if ((head s) == 0) then fromJust $ M.lookup n j
+--                                    else p+1
 
